a8f97dfebb0ce5360a6450dc4621f6e5f0270bc3
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-04-18 19:49:20 -0400

    added time created to admin

--- bgGeoLogger/locations/admin.py
+++ bgGeoLogger/locations/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Coordinates
 
 class CoordinatesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'latitude', 'longitude', 'account')
+    list_display = ('id', 'latitude', 'longitude', 'account', 'time_created')
     list_display_links = ('id',)
 admin.site.register(Coordinates, CoordinatesAdmin)
